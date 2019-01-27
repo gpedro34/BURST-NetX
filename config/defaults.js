8ce909e64ad0bada2b79ef84bddb093f9f422fcc
Gonçalo Pedro <gpedro34@gmail.com>
2019-01-27 10:44:11 +0000

    updated default comments

--- config/defaults.js
+++ config/defaults.js
@@ -3,7 +3,7 @@
 exports.mariaDB = {
   "host": "localhost",          // default: 'localhost'
 	"port": 3306,                 // default: 3306
-	"name": "brs_crawler",        // default: 'NetX'
+	"name": "brs_crawler",        // default: 'brs_crawler'
 	"user": "netX",               // default: 'NetX'
 	"pass": "netX",               // default: 'NetX'
   "maxConnections": 10,         // default: 10
@@ -19,6 +19,6 @@ exports.webserver = {
 exports.brs = {
   "timeout": 10000,             // Default 10 sec
   "userAgent": 'BRS/9.9.9',     // Can be used to test how different wallets talk to each other
-  "peerPort": 8123,             // Default peer port (default 8123) 
+  "peerPort": 8123,             // Default peer port (default 8123)
   "apiPort": 8125               // Default api port (default 8125)
 }
