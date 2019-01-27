df7a3768a64bdb03744767c6a7d1603a775e325c
Shefass <shefass@gmail.com>
2019-01-27 22:19:50 +0200

    ds

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,9 @@ import './App.css';
 var options = { method: 'POST',
   url: 'http://localhost:5000/api/peer',
   headers: 
-   { 'Content-Type': 'application/json'}, 
+   { 'Postman-Token': 'd47c2715-ec6a-450e-9ffd-d9bedd596f22',
+   'cache-control': 'no-cache',
+   'Content-Type': 'application/json'}, 
   body: { id: 1 },
   
   json: true };
@@ -21,18 +23,32 @@ class App extends Component {
     isLoaded: false,
     apiAnswer: ""
    } 
-   
+  this.requestt = this.requestt.bind(this); 
   }
   componentDidMount() {
-    var response = request(options);
-    this.setState({
-      apiAnswer: response
-    })
-   
-    } 
- 
+    var data = JSON.stringify({
+      "id": 1
+    });
+    
+    var xhr = new XMLHttpRequest();
     
     
+    xhr.addEventListener("readystatechange", function () {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
+    
+    xhr.open("POST", "http://127.0.0.1:5000/api/peer");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.setRequestHeader("Postman-Token", "77e454e8-a7eb-456b-978a-54614a6fc0ae");
+    
+    xhr.send(data);
+       } 
+  requestt(){
+  return request(options);
+}   
     
   render() {
    
@@ -40,7 +56,7 @@ class App extends Component {
       return (
 
       <div className="App"> 
-        <p>{console.log(this.apiAnswer)}</p>
+        <p>{console.log()}</p>
         <Table />  
       </div>
     );
