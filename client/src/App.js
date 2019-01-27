1f9dad05ed18cc15d5b5de420864cd5ce6cd7ff1
Shefass <shefass@gmail.com>
2019-01-27 20:45:57 +0200

    First

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,61 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import request from 'request';
 import './App.css';
 
+var options = { method: 'POST',
+  url: 'http://127.0.0.1:5000/api/peer',
+  headers: 
+   {  'Postman-Token': 'ebf4d973-256f-4d20-abfc-2c5217aa8f91',
+   'cache-control': 'no-cache', 'Content-Type': 'application/json'}, 
+  body: { id: 1 },
+  
+  json: true };
+
+
+
+
 class App extends Component {
+  constructor(props){
+  super(props);
+  this.state = {
+    error: null,
+    isLoaded: false,
+    apiAnswer: ""
+   } 
+   
+  }
+  componentDidMount() {
+    var response = request(options);
+    this.setState({
+      apiAnswer: response
+    })
+   
+    } 
+ 
+    
+    
+    
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+   
+  
+      return (
+
+      <div className="App"> 
+        <p>{console.log(this.apiAnswer)}</p>
+        <Table />  
       </div>
     );
+    }
   }
-}
+
 
 export default App;
+
+class Table extends Component {
+  
+  render() {
+         return( <div id="Table">
+
+         </div> );
+  }
+}
