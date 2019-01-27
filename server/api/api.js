9b0a1dc0d75a9d9741333084e5a582405df62226
Shefass <shefass@gmail.com>
2019-01-27 22:26:16 +0200

    wsd

--- server/api/api.js
+++ server/api/api.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser');
 
 //CORS middleware
 const allowCrossDomain = (req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:*');
+    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Methods', 'POST');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
@@ -16,7 +16,7 @@ const allowCrossDomain = (req, res, next) => {
 }
 
 app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({extended:false})); 
 app.use(bodyParser.json());
 app.use(allowCrossDomain);
 
