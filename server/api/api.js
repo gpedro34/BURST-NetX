e9175eabb981f92f6b42b533136492ea9a34aa6b
Gonçalo Pedro <gpedro34@gmail.com>
2019-01-29 10:19:45 +0000

    Public API switch form conf file

--- server/api/api.js
+++ server/api/api.js
@@ -11,7 +11,7 @@ var cors = require('cors');
 const defaults = require('./../../config/defaults').webserver;
 // Whitelists
 let allowedOrigins = defaults.whitelistCORS;
-if(defaults.mode === "DEV" || defaults.mode === "OPEN"){
+if(defaults.mode === "DEV"){
   // Allows mobile and Postman tests
   allowedOrigins.push(undefined);
 }
@@ -23,14 +23,18 @@ if(process.env.CORS_WHITELISTED){
 // CORS SETUP
 const corsOptions = {
   origin: function(origin, callback){
-    // allow requests with no origin
-    // (like mobile apps or curl requests)
-    // if(!origin) return callback(null, true);
-    if(allowedOrigins.indexOf(origin) === -1){
+    if(defaults.mode === 'OPEN'){
+      // Public API - Allow all origins
+      return callback(null, true);
+    } else if(allowedOrigins.indexOf(origin) === -1){
+      // Block Request
       var msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
+    // allow requests with no origin
+    // (like mobile apps or curl requests)
+    // if(!origin) return callback(null, true);
     return callback(null, true);
   }
 }
