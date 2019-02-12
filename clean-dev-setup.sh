1c6450a152d6744da052577e0700cb3e9f254569
gpedro34 <gpedro34@gmail.com>
2019-02-12 15:17:08 +0000

    Corrected scripts for builds of React on Linux- Tested on Ubuntu 18.04

--- clean-dev-setup.sh
+++ clean-dev-setup.sh
@@ -2,8 +2,6 @@
 
 npm cache verify
 
-rm -rf node_modules && rm -rf fe-build && rm ./package-lock.json && npm install && chmod -R a+rwx ./node_modules
-
-cd client && rm -rf node_modules && rm -rf build && npm install && chmod -R a+rwx ./node_modules && cd ..
+rm -rf node_modules && rm -rf fe-build && rm ./package-lock.json && npm install && chmod -R a+rwx ./node_modules && cd client && rm -rf node_modules && rm -rf build && npm install && chmod -R a+rwx ./node_modules && cd ..
 
 exit
