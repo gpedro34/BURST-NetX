643063721f552b69e720571ea64232daf819811e
Gonçalo Pedro <gpedro34@gmail.com>
2019-04-24 20:32:40 +0100

    updated clean-setup.sh

--- clean-setup.sh
+++ clean-setup.sh
@@ -2,6 +2,6 @@
 
 npm cache verify
 
-rm -rf node_modules && rm -rf fe-build && rm ./package-lock.json && npm install && chmod -R a+rwx ./node_modules && cd client && rm -rf node_modules && rm -rf build && npm install && chmod -R a+rwx ./node_modules && cd .. && chmod +x ./build-client.sh && ./build-client.sh
+rm -rf node_modules && rm ./package-lock.json && npm install && chmod -R a+rwx ./node_modules
 
 exit
