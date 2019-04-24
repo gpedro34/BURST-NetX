582fdb5a3bb892f4b03c7dc73906e163622c4442
Gonçalo Pedro <gpedro34@gmail.com>
2019-04-24 20:30:43 +0100

    remove deprecated bash scripts

--- clean-dev-setup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-npm cache verify
-
-rm -rf node_modules && rm -rf fe-build && rm ./package-lock.json && npm install && chmod -R a+rwx ./node_modules && cd client && rm -rf node_modules && rm -rf build && npm install && chmod -R a+rwx ./node_modules && cd ..
-
-exit
