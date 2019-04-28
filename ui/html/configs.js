acf35e5c64056bcfc7d47b39d2037a27778a80a0
Gonçalo Pedro <gpedro34@gmail.com>
2019-04-28 17:36:37 +0100

    linter result changes

--- ui/html/configs.js
+++ ui/html/configs.js
@@ -1,9 +1,12 @@
+'use strict';
+/* eslint prefer-const: "off" */
+/* eslint no-unused-vars: "off" */
 // CONFIGURABLES
 let system = 'WINDOWS'; // 'WINDOWS' or 'LINUX' -> (DEFAULT: 'LINUX')
 let apiDomain = 'BA'; // 'BA' or 'PT' or 'LOCAL' or domain -> (DEFAULT: 'BA')
-let domainPort = 443;
-let useSSL = true;
+const domainPort = 443;
+const useSSL = true;
 
 // Note: you will need to get a mapsApiKey for your project.
 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-let mapsApiKey = 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY';
+const mapsApiKey = 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY';
--- ui/html/js/geochart.js
+++ ui/html/js/geochart.js
@@ -1,29 +1,39 @@
+'use strict';
+/* eslint no-unused-vars: "off" */
+/* eslint no-undef: "off" */
+
 // Launch google GeoCharts API call and map drawing
-function controller(){
-  if(control === 0){
-    document.getElementById("init_div").classList.remove("hide");
-    if(notCountries > 0){
-      console.log('Could not count '+notCountries+' peers as they have no country assigned');
-    }
-    google.charts.load('current', {
-      'packages':['geochart'],
-      'mapsApiKey': mapsApiKey
-    });
-    google.charts.setOnLoadCallback(drawRegionsMap);
-  }
+function controller() {
+	if (control === 0) {
+		document.getElementById('init_div').classList.remove('hide');
+		if (notCountries > 0) {
+			console.log(
+				'Could not count ' +
+					notCountries +
+					' peers as they have no country assigned'
+			);
+		}
+		google.charts.load('current', {
+			packages: ['geochart'],
+			mapsApiKey: mapsApiKey
+		});
+		google.charts.setOnLoadCallback(drawRegionsMap);
+	}
 }
 // Draw map
 function drawRegionsMap() {
-  var data = google.visualization.arrayToDataTable(response);
-  var options = {
-    displayMode: 'auto',
-    datalessRegionColor: '#ffffff',
-    backgroundColor: '#B7C9E8',
-    height: 750,
-    keepAspectRatio: true,
-    colorAxis: {minValue: 0, colors: ['#c4fce4', '#80c15d']},
-    legend: 'none'
-  };
-  var chart = new google.visualization.GeoChart(document.getElementById('regions_div'));
-  chart.draw(data, options);
+	const data = google.visualization.arrayToDataTable(response);
+	const options = {
+		displayMode: 'auto',
+		datalessRegionColor: '#ffffff',
+		backgroundColor: '#B7C9E8',
+		height: 750,
+		keepAspectRatio: true,
+		colorAxis: { minValue: 0, colors: ['#c4fce4', '#80c15d'] },
+		legend: 'none'
+	};
+	const chart = new google.visualization.GeoChart(
+		document.getElementById('regions_div')
+	);
+	chart.draw(data, options);
 }
